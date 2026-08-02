6c0c67a8e76bc3333ac29520ddc0528a6a976d78
asdf57 <53836417+asdf57@users.noreply.github.com>
2026-08-02 01:11:10 -0400

    Make polling interval configurable

--- utils/config.go
+++ utils/config.go
@@ -1,17 +1,46 @@
 package utils
 
 import (
+	"log/slog"
 	"os"
 	"strings"
+	"time"
 )
 
+const DEFAULT_POLLING_INTERVAL = 30 * time.Second
+
 type Config struct {
-	APIEndpoint string
+	APIEndpoint     string
+	PollingInterval time.Duration
 }
 
-func LoadConfig() (Config, error) {
+func LoadConfig(logger *slog.Logger) (Config, error) {
+	var interval time.Duration
+
+	if value := strings.TrimSpace(os.Getenv("POLLING_INTERVAL")); value != "" {
+		var err error
+		interval, err = time.ParseDuration(value)
+		if err != nil {
+			logger.Error(
+				"invalid polling interval",
+				"value", value,
+				"error", err,
+			)
+			logger.Info(
+				"using default polling interval",
+				"interval", DEFAULT_POLLING_INTERVAL,
+			)
+
+			interval = DEFAULT_POLLING_INTERVAL
+		}
+	} else {
+		logger.Info("using default polling interval", "interval", DEFAULT_POLLING_INTERVAL)
+		interval = DEFAULT_POLLING_INTERVAL
+	}
+
 	return Config{
-		APIEndpoint: envOr("API_ENDPOINT", "127.0.0.1:8080"),
+		APIEndpoint:     envOr("API_ENDPOINT", "127.0.0.1:8080"),
+		PollingInterval: interval,
 	}, nil
 }
 

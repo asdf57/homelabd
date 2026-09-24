e530569507662ac1f84c1d1be0cf6f66c8ba2e31
asdf57 <53836417+asdf57@users.noreply.github.com>
2026-09-24 07:19:30 -0400

    let sshd read homelabd managed keys

--- setup/install.sh
+++ setup/install.sh
@@ -62,6 +62,20 @@ sudo install -d \
     -m 0750 \
     /var/lib/homelab/authorized-keys
 
+log "INFO" "Configuring sshd to read homelabd-managed keys"
+sudo install -d -o root -g root -m 0755 /etc/ssh/sshd_config.d
+sudo install \
+    -o root \
+    -g root \
+    -m 0644 \
+    "$REPO_ROOT/setup/sshd/10-homelabd.conf" \
+    /etc/ssh/sshd_config.d/10-homelabd.conf
+if ! sudo sshd -t; then
+    log "ERROR" "The sshd configuration is invalid; removing the homelabd drop-in"
+    sudo rm -f /etc/ssh/sshd_config.d/10-homelabd.conf
+    exit 1
+fi
+
 log "INFO" "Installing systemd service"
 sudo install \
     -o root \
@@ -82,5 +96,8 @@ if ! sudo systemctl restart homelabd.service; then
     sudo journalctl -u homelabd.service --no-pager
     exit 1
 fi
+
+log "INFO" "Reloading sshd"
+sudo systemctl reload sshd.service
 
 log "INFO" "homelabd service is running"
